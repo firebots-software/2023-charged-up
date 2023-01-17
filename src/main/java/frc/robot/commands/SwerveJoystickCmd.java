1907bf5d34fbdc2102e6c4dd639213cda9b43e4e
Ahoys123 <ahoysal855@student.fuhsd.org>
2023-01-16 19:02:04 -0800

    reenabled realtive to field movement

--- src/main/java/frc/robot/commands/SwerveJoystickCmd.java
+++ src/main/java/frc/robot/commands/SwerveJoystickCmd.java
@@ -44,11 +44,13 @@ public class SwerveJoystickCmd extends CommandBase {
         double turningSpeed = turningSpdFunction.get();
 
         // 2. Apply deadband
-        /*
+        double xDir = Math.signum(xSpeed);
+        double yDir = Math.signum(ySpeed);
+        double turnDir = Math.signum(turningSpeed);
+
         xSpeed = Math.abs(xSpeed); //> OI.kDeadband ? xSpeed : 0.0;
         ySpeed = Math.abs(ySpeed); //> OIConstants.kDeadband ? ySpeed : 0.0;
         turningSpeed = Math.abs(turningSpeed); //> OIConstants.kDeadband ? turningSpeed : 0.0;
-        */
 
         // 3. Make the driving smoother
         xSpeed = xLimiter.calculate(xSpeed) * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
@@ -57,15 +59,13 @@ public class SwerveJoystickCmd extends CommandBase {
 
         // 4. Construct desired chassis speeds
         ChassisSpeeds chassisSpeeds;
-        /*
         if (fieldOrientedFunction.get()) {
             // Relative to field
-            chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
-                    xSpeed, ySpeed, turningSpeed, swerveSubsystem.getRotation2d());
-        } else {*/
+            chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, turningSpeed, swerveSubsystem.getRotation2d());
+        } else {
             // Relative to robot
-        chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turningSpeed);
-        //}
+            chassisSpeeds = new ChassisSpeeds(xSpeed * xDir, ySpeed * yDir, turningSpeed * turnDir);
+        }
 
         // 5. Convert chassis speeds to individual module states
         SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);

fd79a3ede464aa8928f97cdc16a81dd6a3aa8008
anthonyl232 <alu232@student.fuhsd.org>
2023-01-28 10:37:33 -0800

    raised fixed turning speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -96,7 +96,7 @@ public final class Constants {
         public static final double kPhysicalMaxSpeedMetersPerSecond = 4.5106;
         public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 2 * 2 * Math.PI;
 
-        public static final double kTeleDriveMaxPercentSpeed = 0.65;
+        public static final double kTeleDriveMaxPercentSpeed = 0.7;
         public static final double kTeleDriveMinPercentSpeed = 0.25;
         public static final double kTeleDriveMaxSpeedMetersPerSecond = kPhysicalMaxSpeedMetersPerSecond * kTeleDriveMaxPercentSpeed;
         public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond = kPhysicalMaxAngularSpeedRadiansPerSecond * kTeleDriveMaxPercentSpeed;

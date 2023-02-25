ef62f5fff20f5144e2739c1547374dcfce04ca49
Ahoys123 <ahoysal855@student.fuhsd.org>
2023-02-25 13:47:52 -0800

    arm renaming + extention

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,126 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import javax.swing.text.StyleContext.SmallAttributeSet;
+
+import org.ejml.dense.row.decomposition.eig.watched.WatchedDoubleStepQREigenvector_FDRM;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.ArmConstants;
+
+public class ArmSubsystem extends SubsystemBase {
+  private static ArmSubsystem instance;
+
+  // rotating
+  private WPI_TalonFX rotatingMotor;
+  private AnalogPotentiometer pot;
+  private Solenoid frictionBreakSolenoid;
+
+  // extending
+  private WPI_TalonFX extendingMotor;
+  private DigitalInput topHall, bottomHall;
+
+  /** Creates a new ArmSubsystem. */
+  public ArmSubsystem() {
+    pot = new AnalogPotentiometer(ArmConstants.POTENTIOMETER_PORT, ArmConstants.RANGE_OF_MOTION,
+        ArmConstants.STARTING_POINT);
+    frictionBreakSolenoid = new Solenoid(PneumaticsModuleType.REVPH,
+        ArmConstants.FRICTION_BREAK_PORT);
+    rotatingMotor = new WPI_TalonFX(ArmConstants.ROTATINGMOTOR_PORT);
+
+    extendingMotor = new WPI_TalonFX(ArmConstants.EXTENDINGMOTOR_PORT);
+    topHall = new DigitalInput(ArmConstants.TOPHALLEFFECT_PORT);
+    bottomHall = new DigitalInput(ArmConstants.BOTTOMHALLEFFECT_PORT);
+  }
+
+  public static ArmSubsystem getInstance() {
+    if (instance == null) {
+      instance = new ArmSubsystem();
+    }
+    return instance;
+  }
+
+  public double _getPotentiometerDegrees() {
+    return pot.get();
+  }
+
+  public void _frictionBreakOn() {
+    if (!_isFrictionBreakOn())
+      frictionBreakSolenoid.set(true);
+  }
+
+  public void _frictionBreakOff() {
+    if (_isFrictionBreakOn())
+      frictionBreakSolenoid.set(false);
+  }
+
+  public boolean _isFrictionBreakOn() {
+    return frictionBreakSolenoid.get();
+  }
+
+  public boolean setRotatingMotor(double speed) {
+    double deg = getRotationDegrees();
+
+    if (deg <= 5)
+      speed = Math.max(speed, 0);
+    else if (deg >= 300)
+      speed = Math.min(speed, 0);
+    
+    // if moving too slow, turn on friction break and don't move
+    if (Math.abs(speed) < ArmConstants.FRICTION_BREAK_DEADBAND) {
+      rotatingMotor.set(0);
+      _frictionBreakOn();
+      return false;
+    }
+
+    // else, set the speed and turn the friction break off
+    _frictionBreakOff();
+    rotatingMotor.set(speed);
+    return true;
+  }
+
+  public void setRotation(double angleDeg) {
+    rotatingMotor.setSelectedSensorPosition(angleDeg / (Constants.ArmConstants.ROTATIONAL_TICKS2ROT * 360.0));
+  }
+
+  public double getRotationDegrees() {
+    return rotatingMotor.getSelectedSensorPosition() * Constants.ArmConstants.ROTATIONAL_TICKS2ROT * 360.0;
+  }
+
+  /*
+   * retracting is negative, extending is positive
+   */
+  public void setExtendingMotor(double speed) {
+    extendingMotor.set(speed);
+  }
+
+  public boolean getTopStatus() {
+    return topHall.get();
+  }
+
+  public boolean getBottomStatus() {
+    return bottomHall.get();
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("potentiometer", _getPotentiometerDegrees());
+    SmartDashboard.putNumber("arm angle", getRotationDegrees());
+  }
+}

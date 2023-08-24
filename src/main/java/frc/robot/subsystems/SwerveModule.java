5b65f4f1c79bbb82c47079dc19b356a8de95ec30
ahoysal <ahoysal855@student.fuhsd.org>
2023-08-24 16:50:09 -0700

    deleting subsystems

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -19,12 +18,19 @@ public class SwerveModule extends SubsystemBase {
     private final WPI_TalonFX turningMotor;
 
     private final CANCoder turningEncoder;
-
     private final PIDController turningPidController;
 
     private final double absoluteEncoderOffsetRad;
-    private final double absoluteDriveEncoderOffset;
 
+    /**
+     * Creates a new SwereModule.
+     * @param driveMotorId the driving motor port #
+     * @param turningMotorId the roatational motor port #
+     * @param CANCoderId the CANCoder port #
+     * @param driveMotorReversed if the drive motor should run "backwards" to increase ticks
+     * @param turningMotorReversed if the turning motor should run "backwards" to increase ticks 
+     * @param absoluteEncoderOffset the encoder offset on the swerve module
+     */
     public SwerveModule(int driveMotorId, int turningMotorId, int CANCoderId, boolean driveMotorReversed, boolean turningMotorReversed, double absoluteEncoderOffset) {
 
         this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
@@ -45,23 +51,12 @@ public class SwerveModule extends SubsystemBase {
 
         turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0);
         turningPidController.enableContinuousInput(-Math.PI, Math.PI);
-
-        absoluteDriveEncoderOffset = driveMotor.getSelectedSensorPosition();
     }
 
-    public double getTurningPosition() {
-        return turningEncoder.getAbsolutePosition() - absoluteEncoderOffsetRad;
-    }
-    
-    public double getTurningVelocity() {
-        return turningEncoder.getVelocity();
-    }
-
-    public SwerveModuleState getState() {
-        // * 10d because getSelectedSensorVelocity() returns ticks/100ms; 
-        return new SwerveModuleState(driveMotor.getSelectedSensorVelocity() * 10d * Constants.ModuleConstants.kDriveEncoderTicks2Meter, new Rotation2d(getTurningPosition()));
-    }
-
+    // setDesiredState attempts to put the swerve drive into the desired state.
+    // this should be run near continuously whenever desired.
+    // possible bug: if set once and left to it's own devices, may continue turning forever.
+    // solution is to call stop, or update setDesiredState near constantly.
     public void setDesiredState(SwerveModuleState state) {
         if (Math.abs(state.speedMetersPerSecond) < 0.001) {
             stop();
@@ -72,48 +67,23 @@ public class SwerveModule extends SubsystemBase {
         turningMotor.set(turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
     }
 
-    public void setTurnState(double toRad) {
-        turningMotor.set(turningPidController.calculate(getTurningPosition(), toRad));
-    }
-
-    public SwerveModulePosition getModulePosition() {
-        return new SwerveModulePosition((driveMotor.getSelectedSensorPosition()-absoluteDriveEncoderOffset) * Constants.ModuleConstants.kDriveEncoderTicks2Meter, getState().angle);
-    }
-
+    // stop sets all motor speeds to zero (stoping any turning or driving).
     public void stop() {
         driveMotor.set(0);
         turningMotor.set(0);
     }
 
-    //private int count = 0;
-
     @Override
-    public void periodic() {
-    /*
-        count++;
-        if (count % 150 == 0) {
-            System.out.println("actual m/s: " + getState().speedMetersPerSecond + "\n.\texpected m/s: " + DEBUG_lastms);
-            System.out.println("power: " + DEBUG_lastms / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
-            //System.out.println("distance m: " + getModulePosition().distanceMeters);
-            count = 0;
-        }
-    */
+    public void periodic() {}
+
+    private double getTurningPosition() {
+        return turningEncoder.getAbsolutePosition() - absoluteEncoderOffsetRad;
     }
 
-    public double getPosition(){
-        return driveMotor.getSelectedSensorPosition();
+    private SwerveModuleState getState() {
+        // * 10d because getSelectedSensorVelocity() returns ticks/100ms; 
+        return new SwerveModuleState(driveMotor.getSelectedSensorVelocity() * 10d * Constants.ModuleConstants.kDriveEncoderTicks2Meter, new Rotation2d(getTurningPosition()));
     }
 
-    public void setMotor(double speed){
-        driveMotor.set(speed);
-    }
-
-    public double getDrivingTickValues(){
-        return (driveMotor.getSelectedSensorPosition()-absoluteDriveEncoderOffset);
-    }
-
-    public void setDrivingTickValues(double val){
-        driveMotor.setSelectedSensorPosition(val);
-    }
 }
 
